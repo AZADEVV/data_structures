de58b44dfdac00fd965a8e8a47d52b677ce3194a
Azamat_Turdubekov <ta12430@auca.kg>
2025-11-05 16:24:32 +0600

    func calculation fix to more correct way

--- complexity/func_calculation.cpp
+++ complexity/func_calculation.cpp
@@ -1,17 +1,32 @@
-#include <cmath>
+// #include <cmath>
+// #include <iostream>
+//
+// int main()
+// {
+//     using namespace std;
+//
+//     int n, f;
+//
+//     cin >> n;
+//
+//     for (int i = 1; i <= n; i++) {
+//         f += pow(-1, i) * i;
+//     }
+//
+//     cout << f << endl;
+// }
+
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    using namespace std;
-
-    int n, f;
-
+int main() {
+    long long n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        f += pow(-1, i) * i;
-    }
+    if (n % 2 == 0)
+        cout << n / 2;
+    else
+        cout << -(n + 1) / 2;
 
-    cout << f << endl;
+    return 0;
 }
