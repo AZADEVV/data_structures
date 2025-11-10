cee9112c2d4c3a2815b5fc498f26a04f3f6cd7e4
Azamat_Turdubekov <ta12430@auca.kg>
2025-11-10 18:14:47 +0600

    win lottery fix

--- complexity/win_lottery.cpp
+++ complexity/win_lottery.cpp
@@ -1,18 +1,44 @@
+// #include <iostream>
+//
+// int main() {
+//     using namespace std;
+//
+//     long long n, min = 0;
+//     int bills[] = {100, 20, 10, 5, 1};
+//     for (int v : bills) {
+//         min += n / v;
+//         n %= v;
+//     }
+//
+//     cout << min << endl;
+//
+//     return 0;
+// }
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    using namespace std;
-
     long long n;
+    cin >> n;
+
     int min = 0;
-    int bills[] = {100, 20, 10, 5, 1};
-    for (const int v : bills) {
-        min += n / v;
-        n %= v;
-    }
+
+    min += n / 100;
+    n %= 100;
+
+    min += n / 20;
+    n %= 20;
+
+    min += n / 10;
+    n %= 10;
+
+    min += n / 5;
+    n %= 5;
+
+    min += n;
 
     cout << min << endl;
 
     return 0;
 }
-
